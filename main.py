593a3bb465713c66b1ae8fd45d2ec8c87dcecddc
Sakib_Dalal <sakibdalal73@gmail.com>
2023-12-30 15:41:30 +0530

    Step 2

--- main.py
+++ main.py
@@ -5,17 +5,35 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def encrypt(plain_text, shift_amount):
     new_text = ""
-    for letter in text:
+    for letter in plain_text:
         if letter == " ":
             new_text += " "
         else:
             position = alphabet.index(letter) + shift_amount
             new_text += alphabet[position]
-    print(f"The encoded text is: {new_text}")
+    print(f"The encrypted text is: {new_text}")
+
+
+def decrypt(encrypt_text, shift_amount):
+    new_text = ""
+    for letter in encrypt_text:
+        if letter == " ":
+            new_text += " "
+        else:
+            position = alphabet.index(letter) - shift_amount
+            new_text += alphabet[position]
+    print(f"The decoded text is: {new_text}")
 
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-encrypt(plain_text=text, shift_amount=shift)
+if direction == "encode":
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    decrypt(encrypt_text=text, shift_amount=shift)
+else:
+    print("Invalid Input!")
