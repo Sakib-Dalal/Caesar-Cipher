4c2f9f28bd60eb5e1d94cda1b05473ac4aa37183
Sakib_Dalal <sakibdalal73@gmail.com>
2023-12-30 15:40:44 +0530

    Step 1

---
+++ main.py
@@ -0,0 +1,21 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def encrypt(plain_text, shift_amount):
+    new_text = ""
+    for letter in text:
+        if letter == " ":
+            new_text += " "
+        else:
+            position = alphabet.index(letter) + shift_amount
+            new_text += alphabet[position]
+    print(f"The encoded text is: {new_text}")
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+encrypt(plain_text=text, shift_amount=shift)
